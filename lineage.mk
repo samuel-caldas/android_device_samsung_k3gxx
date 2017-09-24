58f28e39ab871c31b9eb80863558c225e794b3b4
Martin Bouchet <tincho5588@gmail.com>
2017-09-24 05:53:20 -0300

    lineake.mk: inherit full_base_telephony.mk instead of full_base.mk
    Change-Id: Ia1b5c4a5f9dbb0203ba176f3b4c8f8b5cda2e5a4

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
 # Inherit device configuration
