a0eb19f31adea21cedf8545461009e0352ccf9b9
Griffin98 <griffin98@protonmail.com>
2018-01-25 00:09:43 +0530

    k3gxx: build bluetooth HAL

--- device.mk
+++ device.mk
@@ -37,6 +37,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
+    
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
